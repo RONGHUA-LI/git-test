5ba63450a70f8f0460bb86fc68cfc41b25e3a9da
limiaomiao <limiaomiao@qq.com>
2022-02-06 11:30:16 +0800

    second commit!

--- src/main/java/com/limiaomiao/git/GitTest.java
+++ src/main/java/com/limiaomiao/git/GitTest.java
@@ -3,5 +3,6 @@ package com.limiaomiao.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!");
     }
 }
