b8bb6d748a0f36e68723271c2039efb6ce26d1ab
李喵喵 <48372950+RONGHUA-LI@users.noreply.github.com>
2022-02-06 12:35:17 +0800

    pull test

--- src/main/java/com/limiaomiao/git/GitTest.java
+++ src/main/java/com/limiaomiao/git/GitTest.java
@@ -6,7 +6,7 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git!");
-        System.out.println("push test");
+        System.out.println("pull test");
     }
 }
 
